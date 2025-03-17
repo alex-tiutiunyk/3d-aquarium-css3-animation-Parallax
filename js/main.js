a691efee65cb609fee86f5e37bf87552e511a35d
Alex Tiutiunyk <alex.tiutiunyk@gmail.com>
2025-03-17 10:03:48 +0200

    add generateFish function

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ onMove();
 setInterval(() => {
   generateBubbles();
 }, 500);
+setInterval(() => {
+  generateFish();
+}, 1500);
 
 function randomIntFromInterval(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
@@ -31,19 +34,19 @@ function onMove() {
   });
 }
 
-let count = 0;
+let countBubbles = 0;
 
 function generateBubbles() {
   const bubble = document.createElement('span');
   const container = document.querySelector('.layers__container');
 
-  count++;
+  countBubbles++;
 
   // create element
-  bubble.classList.add('bubble', `bubble-${count}`);
+  bubble.classList.add('bubble', `bubble-${countBubbles}`);
   container.appendChild(bubble);
 
-  const newBubble = document.querySelector(`.bubble-${count}`);
+  const newBubble = document.querySelector(`.bubble-${countBubbles}`);
 
   // add animation
   const x = randomIntFromInterval(0, 100);
@@ -74,4 +77,58 @@ function generateBubbles() {
 
   // remove element
   newBubble.getAnimations()[0].finished.then(() => newBubble.remove());
+}
+
+let countFishes = 0;
+
+function generateFish() {
+  const fish = document.createElement('div');
+  const container = document.querySelector('.layers__container');
+
+  const fishType = randomIntFromInterval(1, 6);
+  const direction = randomIntFromInterval(0, 1);
+  const yStart = randomIntFromInterval(10, 90);
+  const yFinish = randomIntFromInterval(10, 90);
+  const size = Number((randomIntFromInterval(10, 90) * 0.01).toFixed(2));
+  console.log(size);
+  const time = randomIntFromInterval(5000, 30000);
+
+  countFishes++;
+
+  // create element
+  fish.classList.add('fish', `fish-${fishType}`, `fish-count-${countFishes}`);
+  container.appendChild(fish);
+
+  const newFish = document.querySelector(`.fish-count-${countFishes}`);
+
+  if (direction === 0) {
+    newFish.animate(
+      [
+        { transform: `translate(-10vw, ${yStart}vh) translateZ(50px) scale(${size})` },
+        { transform: `translate(110vw, ${yFinish}vh) translateZ(50px) scale(${size + 0.3})` },
+      ],
+      {
+        duration: time,
+      },
+    );
+  } else {
+    newFish.animate(
+      [
+        {
+          transform: `translate(110vw, ${yStart}vh) translateZ(50px) scale(${size}) rotateY(180deg)`,
+        },
+        {
+          transform: `translate(-10vw, ${yFinish}vh) translateZ(50px) scale(${
+            size + 0.3
+          }) rotateY(180deg)`,
+        },
+      ],
+      {
+        duration: time,
+      },
+    );
+  }
+
+  // remove element
+  newFish.getAnimations()[0].finished.then(() => newFish.remove());
 }
